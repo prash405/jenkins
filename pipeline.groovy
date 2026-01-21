9163bbc11b2e92f9b9dab7a9bb4b51ba714b1d6c
prash405 <pprash405@gmail.com>
2026-01-21 14:57:24 +0530

    build added

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,14 @@
 pipeline{   
-    agent any
+    agent {label 'slave2'}
     stages{
-        stage('Build'){
+        stage('git pull'){
             steps{
                 git branch: 'main', url: 'https://github.com/prash405/javaProject.git'
+            }
+        }
+        stage('Build'){
+            steps{
+                echo 'building the application'
             }
         }
         stage('Test'){
